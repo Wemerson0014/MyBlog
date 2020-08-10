d5c984ed2e68918d239ac83fceac4f3e90fdce2c
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2020-08-10 14:32:10 -0300

    Adiciona client para navegabilidade do site, adiciona progress bar para carregamento das páginas, ativa leitura de código javascript no app

--- app/src/main/java/com/estudo/myblog/MainActivity.java
+++ app/src/main/java/com/estudo/myblog/MainActivity.java
@@ -2,8 +2,12 @@ package com.estudo.myblog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +17,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WebView webView = findViewById(R.id.webView);
-        webView.loadUrl("https://www.google.com.br");
+        webView.loadUrl("https://www.tiagoaguiar.co");
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        final ProgressBar progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.INVISIBLE);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressBar.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
