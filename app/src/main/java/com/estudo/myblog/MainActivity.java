dfe186b9530345fd0c2e80e86821f555318a8022
Wemerson Fernandes Rangel <wemersonf.rangel@outlook.com>
2020-08-10 14:42:28 -0300

    Adiciona método para abertura de links externos e suporte a versões anteriores ao android Lollipop

--- app/src/main/java/com/estudo/myblog/MainActivity.java
+++ app/src/main/java/com/estudo/myblog/MainActivity.java
@@ -1,10 +1,15 @@
 package com.estudo.myblog;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
@@ -35,6 +40,31 @@ public class MainActivity extends AppCompatActivity {
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
                 progressBar.setVisibility(View.INVISIBLE);
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                if (request.getUrl().getHost().equals("tiagoaguiar.co")) {
+                    return false;
+                }
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, request.getUrl());
+                startActivity(intent);
+
+                return true;
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (Uri.parse(url).getHost().equals("tiagoaguiar.co")) {
+                    return false;
+                }
+
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+
+                return true;
             }
         });
     }
